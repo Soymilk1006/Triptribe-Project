fd87f475444fd2be760ac8c0df7123c231f16377
DanielQidongOu <danielqidongou@gmail.com>
2023-10-06 20:54:56 +1100

    init: frontend setup

---
+++ src/pages/attractions/[attractionId].tsx
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+const AttractionDetailPage = () => {
+  const router = useRouter();
+  const { attractionId } = router.query;
+  const [attractionData, setAttractionData] = useState(0);
+
+   useEffect(() => {
+    if (attractionId) {
+      setTimeout(() => {
+        setAttractionData(999);
+      }, 1000); 
+    }
+  }, [attractionId]);
+
+  return (
+    <div>
+      <h1>Attraction Detail Page</h1>
+      {attractionData ? (
+        <>
+          <h1>Attraction: {attractionData}</h1> 
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default AttractionDetailPage;
---
+++ src/pages/attractions/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Attractions from '@/sections/attractions/Attractions'
+
+const AttractionsPage = () => {
+  return (
+    <Attractions />
+  );
+};
+
+export default AttractionsPage;
+
+
