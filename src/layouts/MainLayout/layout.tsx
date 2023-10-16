c44095d433910d46afd6c26c80c73ce5766e2564
DanielQidongOu <danielqidongou@gmail.com>
2023-10-16 23:40:32 +1100

    feat:add Box Component

--- src/layouts/MainLayout/layout.tsx
+++ src/layouts/MainLayout/layout.tsx
@@ -1,41 +1,41 @@
 import React, { ReactNode } from 'react';
-import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 
 import Header from './Header';
 import Footer from './Footer';
 
-const LayoutRoot = styled('div')({
-  backgroundColor: '#ffffff',
-  minHeight: '100vh',
-  display: 'flex',
-  flexDirection: 'column',
-  padding: '16px',
-  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-});
-
-const MainContent = styled('div')({
-  flex: 1,
-  display: 'flex',
-});
-
-const Content = styled('div')({
-  flex: 3, 
-  padding: '16px',
-  boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)',
-});
-
-interface LayoutProps {
-  children: ReactNode;
-}
-
-export const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <LayoutRoot>
+    <Box
+      sx={{
+        backgroundColor: '#ffffff',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '16px',
+        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+      }}
+    >
       <Header />
-      <MainContent>
-        <Content>{children}</Content>
-      </MainContent>
+      <Box
+        sx={{
+          flex: 1,
+          display: 'flex',
+        }}
+      >
+        <Box
+          sx={{
+            flex: 3,
+            padding: '16px',
+            boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)',
+          }}
+        >
+          {children}
+        </Box>
+      </Box>
       <Footer />
-    </LayoutRoot>
+    </Box>
   );
 };
+
+export { Layout };
