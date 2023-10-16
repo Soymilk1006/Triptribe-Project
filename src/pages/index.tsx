7454c678e3a5780f6f16b453e991f1286913c86c
DanielQidongOu <danielqidongou@gmail.com>
2023-10-16 23:40:32 +1100

    feat:add Box Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import HomePage from '@/sections/home/HomePage';
-import { Layout } from '@/layouts/MainLayout'
+import { Layout } from '@/layouts/MainLayout';
 
 const Home = () => {
   return (
     <Layout>
       <HomePage />
     </Layout>
-  )
+  );
 };
 
 export default Home;
-
