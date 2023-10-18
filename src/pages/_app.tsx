f9accf75d363dbbed92e24e096e8e06853aa4eb0
NekoLyn <lllynne915@gmail.com>
2023-10-18 23:57:25 +1100

    feat: sign in and sign up
    * add theme provider
    * config server side render mui theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '@/styles/theme';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { EmotionCache } from '@emotion/react';
+import { CacheProvider } from '@emotion/react';
+import { createEmotionCache } from '@/utils/create-emotion-cache';
+
+const clientSideEmotionCache = createEmotionCache();
+export interface TripTribeAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function App({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: TripTribeAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
