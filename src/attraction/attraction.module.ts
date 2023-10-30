594b6ab56d5aac6041212b0d5bffdc3c48ddc777
Russellyyds <954013384@qq.com>
2023-10-31 03:20:24 +1100

    create attraction api v2
    * feat/rebase latest dev
    
    * fix/rebase
    
    * fix/add dayjs and change variable name and set token expire time in constants and remove zod
    
    create attraction api v2
    
    eddit comment
    
    fix indent
    
    fix indent

--- src/attraction/attraction.module.ts
+++ src/attraction/attraction.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AttractionService } from './attraction.service';
 import { AttractionController } from './attraction.controller';
 import { Attraction, AttractionSchema } from './schema/attraction.schema';
+import { FileUploadModule } from '@/file/file.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Attraction.name, schema: AttractionSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Attraction.name, schema: AttractionSchema }]),
+    FileUploadModule],
   controllers: [AttractionController],
   providers: [AttractionService],
 })
-export class AttractionModule {}
+export class AttractionModule { }
