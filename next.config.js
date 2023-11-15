cf4223c466ba9c2932c572333a6f978af9a70124
Devon Li <106891997+Soymilk1006@users.noreply.github.com>
2023-11-15 17:17:45 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+   output: 'export',
+   reactStrictMode: true
 };
 
-module.exports = nextConfig;
+module.exports = {
+  trailingSlash: true,
+};
