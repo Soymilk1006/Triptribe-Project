08e95b8556b528093cb22554fc641a4986df93e3
JamieChen007 <107536976+JamieChen007@users.noreply.github.com>
2023-11-22 00:47:58 +1100

    feat: 67 add users/me api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,29 @@
-import { Controller, Get, Param, UseFilters } from '@nestjs/common';
+import { Controller, Get, Param, UseFilters, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './schema/user.schema';
 import { AllExceptionsFilter } from '@/utils/allExceptions.filter';
-import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from '@/auth/CurrentUser.decorator';
 
-@Controller('user')
-@ApiTags('user')
+@Controller({
+  path: 'users',
+  version: '1',
+})
+@ApiTags('users')
 @UseFilters(AllExceptionsFilter)
 export class UserController {
-  constructor(private readonly appService: UserService) {}
+  constructor(private readonly userService: UserService) {}
+
+  @Get('me')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get me', description: 'Retrieve me successfully' })
+  @ApiResponse({ status: 200, description: 'Retrieve me successfully' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @UseGuards(AuthGuard('jwt'))
+  getMe(@CurrentUser() currentUser): User {
+    return this.userService.getMe(currentUser);
+  }
 
   @Get(':id')
   @ApiOperation({ summary: 'Get an user', description: 'Retrieve an user successfully' })
@@ -21,6 +36,6 @@ export class UserController {
   @ApiResponse({ status: 200, description: 'Retrieve an user by ID successfully' })
   @ApiResponse({ status: 404, description: 'User not found' })
   async find(@Param('id') id: string): Promise<User> {
-    return this.appService.findOne(id);
+    return this.userService.findOne(id);
   }
 }
